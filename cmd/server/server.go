282d1e9c3539a3279036658f4aa91e260e24c610
tinhtn-3406 <tran.ngoc.tinh@sun-asterisk.com>
2024-08-06 16:40:28 +0700

    Setup advance mux server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,17 +2,40 @@ package server
 
 import (
 	"context"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/encoding/protojson"
 	"log"
 	"net"
 	"net/http"
+	"strings"
 
+	"github.com/felixge/httpsnoop"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
 	"grpc-gateway/internal/service"
 	"grpc-gateway/pkg/pb"
 )
+
+func withLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		m := httpsnoop.CaptureMetrics(handler, writer, request)
+		log.Printf("http[%d]-- %s -- %s\n", m.Code, m.Duration, request.URL.Path)
+	})
+}
+
+var allowedHeaders = map[string]struct{}{
+	"x-request-id": {},
+}
+
+func isHeaderAllowed(s string) (string, bool) {
+	// check if allowedHeaders contain the header
+	if _, isAllowed := allowedHeaders[s]; isAllowed {
+		// send uppercase header
+		return strings.ToUpper(s), true
+	}
+	// if not in the allowed header, don't send the header
+	return s, false
+}
 
 func StartGrpcServer() {
 	// Create a listener on TCP port
@@ -31,23 +54,50 @@ func StartGrpcServer() {
 		log.Fatalln(s.Serve(lis))
 	}()
 
-	// Create a client connection to the gRPC server we just started
-	// This is where the gRPC-Gateway proxies the requests
-	conn, err := grpc.NewClient(
-		"0.0.0.0:8080",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	// creating mux for gRPC gateway. This will multiplex or route request different gRPC service
+	gwmux := runtime.NewServeMux(
+		// convert header in response(going from gateway) from metadata received.
+		runtime.WithOutgoingHeaderMatcher(isHeaderAllowed),
+		runtime.WithMetadata(func(ctx context.Context, request *http.Request) metadata.MD {
+			header := request.Header.Get("Authorization")
+			// send all the headers received from the client
+			md := metadata.Pairs("auth", header)
+			return md
+		}),
+		runtime.WithErrorHandler(func(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, writer http.ResponseWriter, request *http.Request, err error) {
+			//creating a new HTTTPStatusError with a custom status, and passing error
+			newError := runtime.HTTPStatusError{
+				HTTPStatus: 400,
+				Err:        err,
+			}
+			// using default handler to do the rest of heavy lifting of marshaling error and adding headers
+			runtime.DefaultHTTPErrorHandler(ctx, mux, marshaler, writer, request, &newError)
+		}),
+		// Custom response default camelCase
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.HTTPBodyMarshaler{
+			Marshaler: &runtime.JSONPb{
+				MarshalOptions: protojson.MarshalOptions{
+					UseProtoNames:   true,
+					EmitUnpopulated: true,
+				},
+				UnmarshalOptions: protojson.UnmarshalOptions{
+					DiscardUnknown: true,
+				},
+			},
+		}),
 	)
-	if err != nil {
-		log.Fatalln("Failed to dial server:", err)
-	}
 
-	gwmux := runtime.NewServeMux()
-	// Register Greeter
-	err = pb.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(1024*1024*20), grpc.MaxCallSendMsgSize(1024*1024*20))}
+
+	// setting up a dail up for gRPC service by specifying endpoint/target url
+	err = pb.RegisterGreeterHandlerFromEndpoint(context.Background(), gwmux, "0.0.0.0:8080", opts)
 	if err != nil {
 		log.Fatalln("Failed to register gateway:", err)
 	}
 
+	// Creating a normal HTTP server
 	gwServer := &http.Server{
 		Addr:    ":8090",
 		Handler: gwmux,
